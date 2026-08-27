a29bdd98251f2022e7c29b097d646566d201a83d
salvomtr <salvomtr@outlook.com>
2026-08-27 16:50:14 +0200

    creation layout

--- views/layout.php
+++ views/layout.php
@@ -1,11 +1,49 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= isset($metaTitle) ? $metaTitle : "" ?> - TomTroc</title>
+    <title><?= isset($metaTitle) ? $metaTitle : "TomTroc" ?></title>
+    <link rel="stylesheet" href="/tomtroc/public/css/style.css">
 </head>
 <body>
-    <?= isset($content) ? $content : "" ?>
+    <header>
+        <div class="header-left">
+            <a href="/tomtroc/" class="logo">
+                <span class="logo-icon">TT</span>
+                Tom Troc
+            </a>
+            <nav>
+                <a href="/tomtroc/">Accueil</a>
+                <a href="/tomtroc/livres">Nos livres à l'échange</a>
+            </nav>
+        </div>
+
+        <div class="header-right">
+            <?php if(isset($_SESSION['user'])): ?>
+                <a href="/tomtroc/messages">Messagerie</a>
+                <a href="/tomtroc/mon-compte">Mon compte</a>
+                <a href="/tomtroc/deconnexion">Déconnexion</a>
+            <?php else: ?>
+                <a href="/tomtroc/connexion">Connexion</a>
+                <a href="/tomtroc/inscription">Inscription</a>
+            <?php endif; ?>
+        </div>
+    </header>
+
+    <main>
+        <?= isset($content) ? $content : "" ?>
+    </main>
+
+    <footer>
+        <nav>
+            <a href="#">Politique de confidentialité</a>
+            <a href="#">Mentions légales</a>
+        </nav>
+        <a href="/tomtroc/" class="logo">
+            Tom Troc&copy;
+            <span class="logo-icon">TT</span>
+        </a>
+    </footer>
 </body>
 </html>
